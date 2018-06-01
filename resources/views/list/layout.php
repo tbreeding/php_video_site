f4fecce44a50779e436936852ccca6d501b4c55e
Sean Hochman <seanhochman@Seans-MacBook-Air.local>
2018-06-01 13:48:33 +0200

    link added

--- resources/views/list/layout.php
+++ resources/views/list/layout.php
@@ -1,9 +1,9 @@
 <div class="container">
 <ul>
     <?php foreach ($song_list as $song) : ?>
-    <li class='listLine'><a href="">
+    <li class='listLine'><a href="<?= $site_url . '?page=addsong&song_id=' . $song->song_id; ?>">
         <p><?php echo $song->song_id; ?></p>
-        <p><?php echo $song->song_title; ?>e</p>
+        <p><?php echo $song->song_title; ?></p>
         <p><?php echo $song->song_author; ?></p>
     </a></li>   
     <?php endforeach; ?>
